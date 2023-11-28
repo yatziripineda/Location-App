d9de0aeaf8c6b40ecab97bfd3e0387dd954f7d64
yatziripineda <131062655+yatziripineda@users.noreply.github.com>
2023-11-28 09:42:20 +0100

    Initial Commit

---
+++ Location App/Location_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_AppApp.swift
+//  Location App
+//
+//  Created by yatziri on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Location_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
